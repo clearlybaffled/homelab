24f8c99f5439c687a7a62c41ea646d2fd5abc9fe
clearlybaffled <clearlybaffled120@gmail.com>
2023-05-30 02:08:31 -0400

    update helm install script to print command before running it

--- scripts/install_helm.sh
+++ scripts/install_helm.sh
@@ -4,6 +4,13 @@ HELM=/usr/local/bin/helm
 JQ=/usr/bin/jq
 VALUES=values.yaml
 KONFIG=kustomization.yaml
-YQ=/home/jared/bin/yq
+YQ=/usr/local/bin/yq
 
-$HELM install --create-namespace -f $VALUES $($YQ -o=json -I=0 '.helmCharts[0]' $KONFIG | $JQ -r --arg name $(basename $PWD) '"\($name) \(.name) --version \(.version) --repo \(.repo) -n \(if .namespace then .namespace else $name end) "')
+# $HELM install --create-namespace -f $VALUES $($YQ -o=json -I=0 '.helmCharts[0]' $KONFIG | $JQ -r --arg name $(basename $PWD) '"\($name) \(.name) --version \(.version) --repo \(.repo) -n \(if .namespace then .namespace else $name end) "')
+
+CMD="$HELM install --create-namespace -f $VALUES "
+CMD+=$($YQ -o=json -I=0 '.helmCharts[0]' $KONFIG | $JQ -r --arg name $(basename $PWD) '"\($name) \(.name) --version \(.version) --repo \(.repo) -n \(if .namespace then .namespace else $name end) "')
+
+echo "Command is: $CMD"
+
+exec $CMD
