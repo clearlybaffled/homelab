f608f6544f3c2cb2251be8d78aff15d2771b82d3
clearlybaffled <clearlybaffled120@gmail.com>
2023-06-07 06:07:17 -0400

    Complete restructure (again...)
    - Apps listed in cluster/bootstrap
    - Everything in infra/roles/cluster moved to cluster/boostrap or purged
    - Rook/Ceph implementated

--- scripts/install_helm.sh
+++ scripts/install_helm.sh
@@ -8,9 +8,16 @@ YQ=/usr/local/bin/yq
 
 # $HELM install --create-namespace -f $VALUES $($YQ -o=json -I=0 '.helmCharts[0]' $KONFIG | $JQ -r --arg name $(basename $PWD) '"\($name) \(.name) --version \(.version) --repo \(.repo) -n \(if .namespace then .namespace else $name end) "')
 
+if [ $# -ne 0 ]
+then
+  pushd $1 &>/dev/null
+fi
+
 CMD="$HELM install --create-namespace -f $VALUES "
 CMD+=$($YQ -o=json -I=0 '.helmCharts[0]' $KONFIG | $JQ -r --arg name $(basename $PWD) '"\($name) \(.name) --version \(.version) --repo \(.repo) -n \(if .namespace then .namespace else $name end) "')
 
 echo "Command is: $CMD"
 
 exec $CMD
+
+popd &>/dev/null
